fcc500408bcd5ac60735a824e6914dca6642fc5c
Mike Douglas <mjd@athinkingape.com>
2013-01-05 13:11:54 -0800

    Don't autocorrect sudo commands.
    The arguments are likely commands, not files.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,5 @@ alias ls='gls $LS_OPTIONS -hF'
 alias ll='gls $LS_OPTIONS -lhF'
 alias l='gls $LS_OPTIONS -lAhF'
 
-alias love="/Applications/love.app/Contents/MacOS/love"
+# avoid attempts to auto-correct 'vim' to '.vim'
+alias sudo='nocorrect sudo'
